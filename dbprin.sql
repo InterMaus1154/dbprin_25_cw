d64eed17c34797942a7e426712994f7245766b64
josh098900 <joshmathers07@icloud.com>
2025-10-22 20:42:16 +0100

    initial structure and ds store added to ignore because mac lol

---
+++ dbprin.sql
@@ -0,0 +1,73 @@
+CREATE TABLE cities;
+
+CREATE TABLE suppliers;
+
+CREATE TABLE part_categories;
+
+CREATE TABLE parts;
+
+CREATE TABLE part_suppliers;
+
+CREATE TABLE packages;
+
+CREATE TABLE services;
+
+CREATE TABLE package_services;
+
+CREATE TABLE service_discounts;
+
+CREATE TABLE memberships;
+
+CREATE TABLE membership_services;
+
+CREATE TABLE customers;
+
+CREATE TABLE emergency_contacts;
+
+CREATE TABLE vehicle_brands;
+
+CREATE TABLE vehicles;
+
+CREATE TABLE branches;
+
+CREATE TABLE bays;
+
+CREATE TABLE staff;
+
+CREATE TABLE staff_schedule;
+
+CREATE TABLE staff_certifications;
+
+CREATE TABLE branch_managers;
+
+CREATE TABLE roles;
+
+CREATE TABLE staff_roles;
+
+CREATE TABLE bay_inspections;
+
+CREATE TABLE bookings;
+
+CREATE TABLE invoices;
+
+CREATE TABLE installments;
+
+CREATE TABLE refunds;
+
+CREATE TABLE booking_services;
+
+CREATE TABLE jobs;
+
+CREATE TABLE additional_services;
+
+CREATE TABLE branch_parts;
+
+CREATE TABLE part_usage;
+
+CREATE TABLE part_transfers;
+
+CREATE TABLE mot_results;
+
+CREATE TABLE customer_feedbacks;
+
+CREATE TABLE feedback_replies;
