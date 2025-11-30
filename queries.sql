9ed2807221bdb7abe512e41e9350698183a47f80
intermaus1154 <amarco1154@gmail.com>
2025-11-30 13:14:36 +0000

    move all queries to queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,114 @@
+-- QUERY 1
+-- Show MOTs that are either expired, or will expire in the next 7 or 30 days
+-- For identification it includes vehicle reg, customer full name, and customer contact number, as well as the expiry date
+WITH latest_mot AS (SELECT vec_id,
+                           expiry_date,
+                           ROW_NUMBER() OVER (
+                               PARTITION BY vec_id
+                               ORDER BY expiry_date DESC
+                               ) AS rank
+                    FROM mot_results
+                    WHERE expiry_date <= CURRENT_DATE + INTERVAL '30 days')
+SELECT v.vec_reg                                  AS "Registration",
+       CONCAT_WS(' ', c.cust_fname, c.cust_lname) AS "Customer",
+       c.cust_contact_num                         AS "Contact Number",
+       mt.expiry_date                             AS "Expiry",
+       CASE
+           WHEN mt.expiry_date <= CURRENT_DATE THEN 'EXPIRED'
+           WHEN mt.expiry_date <= CURRENT_DATE + INTERVAL '7 days' THEN 'EXPIRE IN 7 DAYS'
+           WHEN mt.expiry_date <= CURRENT_DATE + INTERVAL '30 days' THEN 'EXPIRE IN 30 DAYS'
+           END                                    AS "Status"
+FROM latest_mot mt
+         JOIN vehicle_safe v
+              USING (vec_id)
+         JOIN customer_safe c
+              USING (cust_id)
+WHERE mt.rank = 1
+ORDER BY expiry_date DESC, "Status" ASC;
+
+-- END QUERY 1
+
+-- QUERY 2
+-- performance per branch in the year of 2025
+-- ranking over branches based on total income (includes already paid, and unpaid invoices)
+-- total number of bookings
+-- total income per branch, paid invoices only
+-- % of invoices that are unpaid, compared to total count of invoices per branch
+-- missing (unpaid) invoices in GBP
+-- total jobs completed in the branch
+-- the staff who completed the most number of jobs
+-- the number of jobs completed by that staff,
+-- ordered by the branch rank, from highest to lowest
+WITH filtered_bookings AS (SELECT booking_id, branch_id
+                           FROM bookings
+                           WHERE booking_date >= '2025-01-01'
+                             AND booking_date <= '2025-12-31'),
+     invoice_data AS (SELECT booking_id,
+                             SUM(inv_final) FILTER ( WHERE inv_status = 'PAID' ) AS paid_inv_final,
+                             SUM(inv_final) FILTER (WHERE inv_status != 'PAID')  AS due_inv_final,
+                             COUNT(*)                                            AS total_invoices,
+                             COUNT(*) FILTER (WHERE inv_status != 'PAID')        AS due_count
+                      FROM invoices
+                      GROUP BY booking_id),
+     job_data AS (SELECT bs.booking_id AS booking_id,
+                         COUNT(*)      AS completed_jobs
+                  FROM booking_services bs
+                           JOIN jobs j
+                                ON j.booking_service_id = bs.booking_service_id
+                  WHERE j.job_status = 'COMPLETED'
+                    AND j.job_end >= '2025-01-01'
+                    AND j.job_end <= '2025-12-31'
+                  GROUP BY bs.booking_id),
+     staff_data AS (SELECT s.branch_id,
+                           j.staff_id,
+                           COUNT(*) AS staff_jobs_no
+                    FROM jobs j
+                             JOIN staff s
+                                  USING (staff_id)
+                    WHERE j.job_status = 'COMPLETED'
+                      AND j.job_end >= '2025-01-01'
+                      AND j.job_end <= '2025-12-31'
+                    GROUP BY s.branch_id, j.staff_id),
+     top_staff AS (SELECT branch_id, staff_id, staff_jobs_no
+                   FROM (SELECT branch_id,
+                                staff_id,
+                                staff_jobs_no,
+                                ROW_NUMBER() OVER (
+                                    PARTITION BY branch_id
+                                    ORDER BY staff_jobs_no DESC
+                                    ) AS rn
+                         FROM staff_data) ranked
+                   WHERE rn = 1)
+SELECT RANK()
+       OVER (ORDER BY SUM(COALESCE(id.paid_inv_final, 0) + COALESCE(id.due_inv_final, 0)) DESC) AS "Branch Total Income Rank",
+       CONCAT_WS(' ', b.branch_code, b.branch_name)                                             AS "Branch",
+       COUNT(DISTINCT fb.booking_id)                                                            AS "No. of Branch Bookings",
+       COALESCE(ROUND(SUM(paid_inv_final), 2), 0)                                               AS "Total Branch Income (GBP)",
+       CONCAT(COALESCE(ROUND(SUM(id.due_count) * 100.00 / NULLIF(SUM(id.total_invoices), 0), 2),
+                       0),
+              '%')                                                                              AS "Unpaid Invoices / Total Invoices (%)",
+       COALESCE(ROUND(SUM(id.due_inv_final), 2), 0)                                             AS "Missing Income from Unpaid Invoices (GBP)",
+       COALESCE(SUM(jd.completed_jobs), 0)                                                      AS "Total Completed Branch Jobs",
+       COALESCE(MAX(CONCAT_WS(' ', s.staff_fname, s.staff_lname)),
+                'N/A')                                                                          AS "Most Jobs Completed By",
+       COALESCE(MAX(ts.staff_jobs_no), 0)                                                       AS "Staff Completed Jobs"
+FROM branches b
+         LEFT JOIN filtered_bookings fb
+                   ON fb.branch_id = b.branch_id
+         LEFT JOIN invoice_data id
+                   ON id.booking_id = fb.booking_id
+         LEFT JOIN job_data jd
+                   ON jd.booking_id = fb.booking_id
+         LEFT JOIN top_staff ts
+                   ON ts.branch_id = b.branch_id
+         LEFT JOIN staff s
+                   ON s.staff_id = ts.staff_id
+GROUP BY b.branch_name, b.branch_code
+ORDER BY "Branch Total Income Rank",
+         "No. of Branch Bookings" DESC,
+         "Total Branch Income (GBP)" DESC;
+
+-- Query 3
 SELECT
     -- Customer Info (cust email and name to send reminders abour mship)
     c.cust_id                               as id,
@@ -55,30 +166,21 @@ GROUP BY c.cust_id,
          m.mship_name
 ORDER BY total_spend DESC;
 
+-- Query 4
 -- Branch Level Workload
 SELECT b.branch_name,
-       COUNT(DISTINCT s.staff_id) as total_staff,
-       COUNT(j.job_id)            as total_jobs,
-       ROUND(
-               COUNT(j.job_id) :: NUMERIC / NULLIF(COUNT(DISTINCT s.staff_id), 0),
-               1
-       )                          as avg_jobs_per_staff,
-       COUNT(
-               CASE
-                   WHEN j.job_status = 'SCHEDULED' THEN 1
-                   END
-       )                          as pending_jobs,
-       COUNT(
-               CASE
-                   WHEN j.job_status = 'IN_PROGRESS' THEN 1
-                   END
-       )                          as active_jobs
+       COUNT(DISTINCT s.staff_id)                                                   as total_staff,
+       COUNT(j.job_id)                                                              as total_jobs,
+       ROUND(COUNT(j.job_id) :: NUMERIC / NULLIF(COUNT(DISTINCT s.staff_id), 0), 0) as avg_jobs_per_staff,
+       COUNT(j.job_id) FILTER ( WHERE j.job_status = 'SCHEDULED' )                  as pending_jobs,
+       COUNT(j.job_id) FILTER ( WHERE j.job_status = 'IN_PROGRESS' )                as active_jobs
 FROM branches b
          INNER JOIN staff s ON b.branch_id = s.branch_id
          LEFT JOIN jobs j ON s.staff_id = j.staff_id
 GROUP BY b.branch_name
 ORDER BY total_jobs DESC;
 
+-- Query 5
 SELECT p.part_name,
        pc.part_cat_name,
        bp.branch_id,
@@ -118,11 +220,11 @@ ORDER BY part_id;
 -- Another simple example as a data analyst
 -- Showing the number of bookings, where customer left a feedback
 -- Including percentage
-WITH booking_count_with_feedbacks
+WITH booking_count_with_feedbacks -- separately count the bookings with feedbacks
          AS (SELECT COUNT(*) FILTER ( WHERE EXISTS (SELECT 1
                                                     FROM customer_feedback_analyst cfa
                                                     WHERE cfa.booking_id = b.booking_id) ) as count
              FROM bookings b)
 SELECT (SELECT count FROM booking_count_with_feedbacks)       AS "Bookigns with feedbacks",
        CONCAT(ROUND(((SELECT count FROM booking_count_with_feedbacks)::NUMERIC
-           / (SELECT count(*) FROM bookings)) * 100, 2), '%') AS "% of bookings with feedback";
+           / (SELECT count(*) FROM bookings)) * 100, 2), '%') AS "% of bookings with feedback"; -- calculate percentage
